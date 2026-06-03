a15b0f1ba39b6a78d92f167f8e95d73af8a1c641
Viktor Volokh <volohviktor1@gmail.com>
2026-06-03 13:01:38 +0200

    practice math functions

---
+++ module_math_practice.py
@@ -0,0 +1,31 @@
+from math import *
+"""
+a = float(input())
+b = float(input())
+c = log10(pow(a, pi)-sin(pow(b, 3) + 3 * pi / 2))
+print(c)
+for i in range(1, 101):
+    a, b = i, i
+    c = log10(pow(a, pi) - sin(pow(b, 3) + 3 * pi / 2))
+    print(c)
+"""
+"""
+k = float(input())
+x = float(input())
+E = (k * pow(x, 2)) / 2
+m = float(input())
+v = sqrt(2*E/m)
+print(v)
+"""
+F = float(input())
+S = float(input())
+W = float(input())
+a = asin(W / (F * S))
+print(a)
+#other functions:
+#log(n, base)
+#ceil(num) 6.1 -> 7
+#floor(num): 6.1 -> 6
+#abs(num)
+#min(num)
+#max(num)
